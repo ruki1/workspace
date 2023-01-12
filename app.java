a6a935ad6873f96134ed461673840dbeb287fc63
Rukiyat Adams <105608433+ruki1@users.noreply.github.com>
2023-01-12 15:07:01 -0800

    Update app.java

--- app.java
+++ app.java
@@ -3,5 +3,5 @@ best practice leads to good code
 good code leads to good software
 good software leads to happy customers
 happy customers leads to more money
-more money leads to more happiness
-more happiness leads to more money
+more money leads to more happiness when God is involved
+more happiness leads to more money for sure!
