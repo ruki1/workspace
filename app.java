65a4c29edba080d16f29466bd92fceaf8eaa7e1f
ruki <alliruki@gmail.com>
2023-01-12 14:47:10 -0800

    my first commit

---
+++ app.java
@@ -0,0 +1,7 @@
+Always use best practice 
+best practice leads to good code
+good code leads to good software
+good software leads to happy customers
+happy customers leads to more money
+more money leads to more happiness
+more happiness leads to more money
